16f2e79650f0aea1ff537ced12eb2d07cceedfd7
Jose Rezende <rezendinhobr18@gmail.com>
2022-10-31 23:13:09 -0300

    VersionBeta

--- TestingAPIDolar.py
+++ TestingAPIDolar.py
@@ -21,12 +21,13 @@ print( "Euro: ","R$:",item2)
 print( "Bitcoin: ","R$:",item3)
 font = ("Arial", 12)
 sg.theme('DarkBlue')
-layout = [  [sg.Text('Price of Dollar',font=font,),sg.Text('R$:', font=font),sg.Text(item1, font=font)],    # type: ignore
-            [sg.Text('Price of Euro', font=font),sg.Text('R$:', font=font),sg.Text(item2,font=font)],    # type: ignore
-            [sg.Text('Price of Coin',font=font),sg.Text('R$:', font=font),sg.Text(item3,font=font)],    # type: ignore
+layout = [  [sg.Text('Value Converted To Reais',font=font)],
+            [sg.Text('Price of Dollar R$:',font=font),sg.Text(item1,size=(20, 1),key='-text-',font=font)],           # type: ignore
+            [sg.Text('Price of Euro   R$:',font=font),sg.Text(item2,size=(20, 1),key='-text-',font=font)],           # type: ignore
+            [sg.Text('Price of Coin   R$:',font=font),sg.Text(item3,size=(20, 1),key='-text-',font=font)],           # type: ignore
             [sg.Button('Close',size=(200, 1), key='-text-', font=font,button_color = 'Yellow')],
-            [sg.Text(dt,size=(200, 1), key='-text-',justification='center')] ] # type: ignore 
-window = sg.Window('Quote Table', layout,size=(300, 160), element_justification='center')
+            [sg.Text(dt,size=(200, 1), key='-text-',justification='center')]  ]                                                                                 # type: ignore 
+window = sg.Window('Quote Table', layout,size=(300, 180), element_justification='center')
 while True:
     event, values = window.read()  # type: ignore
     if event == sg.WIN_CLOSED or event == 'Close':
